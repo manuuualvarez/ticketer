3b224079f3b3a090df3a7a913a5861c05ad85f87
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2024-11-29 02:21:07 -0300

    Allow the domain to see images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { hostname: "superb-dolphin-382.convex.cloud", protocol: "https" },
+      // TODO: Add your custom domain here
+      { hostname: "cheery-ibis-811.convex.cloud", protocol: "https" },
     ],
   },
 };
